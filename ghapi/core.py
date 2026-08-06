b545ddca7bb09cf73b6cf7125ead4663f49d0cc7
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-06 20:13:16 +1000

    fixes #227

--- ghapi/core.py
+++ ghapi/core.py
@@ -242,11 +242,11 @@ async def upload_file(self:GhApi, rel, fn):
 
 # %% ../nbs/00_core.ipynb #3cad71a4
 @gh_patch
-async def create_release(self:GhApi, tag_name, branch='master', name=None, body='',
+async def create_release(self:GhApi, tag_name, branch=None, name=None, body='',
     draft=False, prerelease=False, files=None, make_latest=UNSET):
-    "Wrapper for `GhApi.repos.create_release` which also uploads `files`"
+    "Wrapper for `GhApi.repos.create_release` which also uploads `files`; `branch` defaults to the repo's default branch"
     if name is None: name = 'v'+tag_name
-    rel = await self.repos.create_release(tag_name, target_commitish=branch, name=name, body=body,
+    rel = await self.repos.create_release(tag_name, target_commitish=branch or UNSET, name=name, body=body,
         draft=draft, prerelease=prerelease, make_latest=make_latest)
     for file in listify(files): await self.upload_file(rel, file)
     return rel
